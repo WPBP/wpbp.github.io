fd18eacc5340132f6f3fbc1385593b1229a9d9f2
Daniele Scasciafratte <mte90net@gmail.com>
2018-10-08 18:50:25 +0200

    improving page load

--- js/main.js
+++ js/main.js
@@ -4,16 +4,13 @@ $(document).ready(function() {
 
     //Page loader
     if ($('.pageloader').length) {
-
         $('.pageloader').toggleClass('is-active');
 
-        $(window).on('load', function() {
-            var pageloaderTimeout = setTimeout( function() {
-                $('.pageloader').toggleClass('is-active');
-                $('.infraloader').toggleClass('is-active')
-                clearTimeout( pageloaderTimeout );
-            }, 700 );
-        })
+        var pageloaderTimeout = setTimeout( function() {
+            $('.pageloader').toggleClass('is-active');
+            $('.infraloader').toggleClass('is-active')
+            clearTimeout( pageloaderTimeout );
+        }, 700 );
     }
 
     //Navbar Clone
